0ea26baabea71e144cb7385a021bc264dc4690d4
Colin Sagan <careof@colinsagan.com>
2020-05-29 21:57:15 +0800

    Add authentication and apply it to invites service.

--- src/services/invites/invites.hooks.js
+++ src/services/invites/invites.hooks.js
@@ -1,14 +1,35 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const { setField } = require('feathers-authentication-hooks');
 
+const setUserId = setField({
+  from: 'params.user._id',
+  as: 'data.userId'
+});
+
+const limitToUser = setField({
+  from: 'params.user._id',
+  as: 'params.query.userId'
+});
 
 module.exports = {
   before: {
-    all: [],
+    all: [
+      authenticate('jwt'),
+    ],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [
+      setUserId,
+    ],
+    update: [
+      limitToUser,
+    ],
+    patch: [
+      limitToUser,
+    ],
+    remove: [
+      limitToUser,
+    ]
   },
 
   after: {
